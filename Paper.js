abbccccad4a09e408f22520398c7ad695e1352d3
JanM123 <65114580+JanM123@users.noreply.github.com>
2020-07-10 08:13:50 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,24 @@
+class Paper {
+
+constructor(x, y, radius) {
+   var options = {
+   	   'isStatic': false,
+       'restituition': 0.3,
+       'friction': 0.5,
+       'density': 1.2
+   };
+
+   this.body = Bodies.circle(x, y, radius, options);
+   this.radius = radius;
+
+   World.add(world, this.body);
+}
+
+display() {
+    var pos = this.body.position;
+    push();
+    rectMode(CENTER);
+    fill("white");
+    circle(pos.x, pos.y, this.radius);
+}
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var side1, side2, side3;
+var paper;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(400, 675, 800, 50);
+	World.add(world, ground);
+
+	side1 = new Dustbin(600, 645, 190, 15);
+	World.add(world, side1);
+
+	side2 = new Dustbin(700, 595, 15, 100);
+	World.add(world, side2);
+
+	side3 = new Dustbin(500, 595, 15, 100);
+	World.add(world, side3);
+
+	paper = new Paper(200, 450, 25);
+	World.add(world, paper);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ground.display();
+
+  side1.display();
+  side2.display();
+  side3.display();
+
+  paper.display();
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+	Matter.Body.applyForce(paper.body, paper.body.position, {x: 85, y:-85});
+  }
+
+}
+
